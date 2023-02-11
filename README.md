f25567e50dc2ad3ebe83f0cf78d7b8a79974b881
Theron Joe <theronj@joetwebdev.io>
2023-02-10 23:11:43 -0700

    setup html page, added input. added tailwindcss. setting up routes. still need to add color/upload class, wip.

---
+++ README.md
@@ -0,0 +1,6 @@
+# project-red-rock
+
+1. yarn install
+2. yarn -i ./static/src/input.css -o ./static/dist/css/output.css
+3. third
+
--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, flash, request, render_template
 
 app = Flask(__name__)
 
 # for basic color extraction, use colorgram
 # for advance usage, graphs, set number of colors, matplotlib and pandas
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html')
 
-@app.route("/about")
+@app.route('/about')
 def about():
-    return "<p>About</p>"
+    return '<p>About</p>'
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        flash('No file part')
+    return '<p>About</p>'
