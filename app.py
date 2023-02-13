d0312f48087f7fec058a0e6d7d919e05d2737d98
Theron Joe <theronj@joetwebdev.io>
2023-02-13 03:07:20 -0700

    added more validation of files on upload. added a method to convert rgb to hex. added flash messaging and implemented it to the html file. add js functions for copy hex code and returning current year, also a method for setting the message returned by the copy method. added a description of the color extractor. updated some of the styles. clean up and prepare to deploy, wip.

--- app.py
+++ app.py
@@ -1,35 +1,45 @@
 import os
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, flash, url_for, render_template
 from werkzeug.utils import secure_filename
 import colorgram
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = './static/uploads/'
+# UPLOAD_FOLDER = './static/uploads/'
+UPLOAD_FOLDER = '/tmp/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'secret_key'
 
 # for basic color extraction, use colorgram
 # for advance usage, graphs, set number of colors, matplotlib and pandas
 
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def rgb_to_hex(r, g, b):
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return '<p>About</p>'
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         file = request.files["file"]
         if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('show_colors', filename=filename))
+            if allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return redirect(url_for('show_colors', filename=filename))
+            else:
+                flash('File type is not approved.')
+                return redirect(url_for('index'))
         else:
-            message = 'No file uploaded'
-        return render_template('index.html', error=message)
+            flash('No file uploaded')
+            return redirect(url_for('index'))
     else:
         return render_template('index.html')
 
@@ -38,7 +48,7 @@ def upload():
 def show_colors(filename):
     file_location = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     colors = colorgram.extract(file_location, 6)
-    return render_template('basic_show.html', colors=colors)
+    return render_template('basic_show.html', colors=colors, hex=rgb_to_hex)
 
 
 if __name__ == '__main__':
